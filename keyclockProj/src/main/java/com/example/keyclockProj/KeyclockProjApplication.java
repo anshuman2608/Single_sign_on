22e543de1765226d30ed1e4618b0a5ea27ba9307
anshuman2608 <asingh851127@gmail.com>
2024-04-27 18:56:00 +0530

    keycloak for authentication and authorization using springboot

---
+++ keyclockProj/src/main/java/com/example/keyclockProj/KeyclockProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.keyclockProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyclockProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyclockProjApplication.class, args);
+	}
+
+}
---
+++ keyclockProj/src/test/java/com/example/keyclockProj/KeyclockProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keyclockProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyclockProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
